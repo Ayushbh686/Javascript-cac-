08b3799203297210ea377b4aa24cee56b7ee8445
Ayushbh686 <159467620+Ayushbh686@users.noreply.github.com>
2024-04-14 23:23:54 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,16 @@
 // console.log("2" > 1);  //true  (javascrpit convert automatically)
 // console.log("02" > 1);  //true 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true (comparision operator converts automatically)
+console.log("02" === 2); //false strict check
+console.log("02" == 2); //true (comparision operator converts automatically)
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);  //false
+console.log(undefined > 0);   //false
+console.log(undefined < 0);   //false
 
-// === 
+// === check values strictly with data types
 
 console.log("2" === 2);
