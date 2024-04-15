66decc3ac9a641c4f73e7c67af9ca2c05371cae2
Ayushbh686 <159467620+Ayushbh686@users.noreply.github.com>
2024-04-15 11:51:19 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,12 +1,17 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// let mydate= new Date()
+// console.log(mydate);  //2024-04-15T05:55:45.859Z
+// console.log(mydate.toString()); //Mon Apr 15 2024 11:25:45 GMT+0530 (India Standard Time)
+// console.log(mydate.toDateString()); //Mon Apr 15 2024
+// console.log(mydate.toISOString);  //[Function: toISOString]
+// console.log(mydate.toJSON());  //2024-04-15T05:55:45.859Z
+// console.log(mydate.toLocaleDateString());  //15/4/2024
+// console.log(mydate.toLocaleString());  //15/4/2024, 11:25:45 am
+
+// let myCreatedDate = new Date(2023, 0, 23)   // months start from zero monday 23 jan 2023
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
