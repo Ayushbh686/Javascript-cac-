0130abe95f62350b870e53208517dd16d57c3d9b
Ayushbh686 <159467620+Ayushbh686@users.noreply.github.com>
2024-04-18 23:11:50 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object()  //singleton 
+const tinderUser = {}  //non singleton
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -24,7 +24,7 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  //first is target and after that are source
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
@@ -48,9 +48,9 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));  //arrays
 // console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));  //arrays of array (key value pair)
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
